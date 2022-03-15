c122825c0aa737248d8b741a91f23eee68400aa5
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-15 14:48:11 +0100

    bonus complete

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
-let righe = 10;
-let colonne = 10;
-let totCell = righe * colonne;
+let righe;
+let colonne;
+
 const numeriEstratti = [];
-const difficolta = parseInt(console.log("scegli difficolta da 1 a 3"));
+let difficolta = parseInt(prompt("scegli difficolta da 1 a 3"));
+while (difficolta < 1 || difficolta > 3 || isNaN(difficolta)) {
+    difficolta = parseInt(prompt("scegli difficolta da 1 a 3"));
+}
+if (difficolta == 1) {
+    //con difficoltà 1 => tra 1 e 100
+    righe = 10;
+    colonne = 10;
+} else if (difficolta == 2) {
+    //con difficoltà 2 => tra 1 e 81
+    righe = 9;
+    colonne = 9;
+} else if (difficolta == 3) {
+    //con difficoltà 3 => tra 1 e 49
+    righe = 7;
+    colonne = 7;
+}
 
-// if (difficolta == 1) {
-
-// } else if (difficolta == 2) {
-
-// }
+let totCell = righe * colonne;
 function generateRandomNumber(min, max) {
     const range = (max - min) + 1;
     const numeroRandom = Math.floor(Math.random() * range + min);
     return numeroRandom;
 }
-
-function creazioneCelle() {
+function creazioneCelle(difficolta) {
     const element = document.createElement("div");
     element.classList.add("cell")
+    if (difficolta == 1) {
+        element.classList.add("difficolta-1");
+    } else if (difficolta == 2) {
+        element.classList.add("difficolta-2");
+    } else if (difficolta == 3) {
+        element.classList.add("difficolta-3");
+    }
     return element;
+
 }
 function controlloNumeri(min, max, registro) {
     let result = generateRandomNumber(min, max)
@@ -27,10 +46,9 @@ function controlloNumeri(min, max, registro) {
     }
     return result;
 }
-
 for (let index = 0; index < totCell; index++) {
     const grid = document.getElementById("Grid");
-    const cell = creazioneCelle();
+    const cell = creazioneCelle(difficolta);
     const random = controlloNumeri(1, totCell, numeriEstratti);
     numeriEstratti.push(random);
     cell.innerText = random;
